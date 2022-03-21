30948f182e1891c7c08d571c938a955b21f4d91a
Darrik Houck <darrikwh@gmail.com>
2022-03-21 11:35:02 -0700

    need to fix auth

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 const { google } = require("googleapis");
+
 const express = require("express");
 const app = express();
 const port = 5000;
 
+const auth = google.auth.getClient({
+  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+});
+
+const sheets = google.sheets({ version: "v4", auth });
+
+const range = "Sheet1!A2:B3";
+
+async function getEvents() {
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range,
+  });
+
+  return response;
+}
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/test", (req, res) => {
   res.status(200);
-  res.send("testtest");
+  res.send(getEvents());
   console.log("success");
 });
