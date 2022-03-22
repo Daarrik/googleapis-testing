2335a9b484e6a9507fe68686d6fca80ca7470dd9
Daarrik <darrikwh@gmail.com>
2022-03-22 00:26:33 -0700

    clean for next apis auth attempt

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,6 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const auth = google.auth.getClient({
-  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-});
-
-const sheets = google.sheets({ version: "v4", auth });
-
-const range = "Sheet1!A2:B3";
-
-async function getEvents() {
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SHEET_ID,
-    range,
-  });
-
-  return response;
-}
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/test", (req, res) => {
