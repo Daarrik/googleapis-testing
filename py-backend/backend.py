b7e6176c942deebcbdca53ffc62c20639ddde125
Darrik Houck <darrikwh@gmail.com>
2022-04-07 23:33:44 -0700

    Flask backend working

--- py-backend/backend.py
+++ py-backend/backend.py
@@ -1,57 +1,31 @@
-from __future__ import print_function
-import os.path
 from flask import Flask
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+  CREDENTIALS = 'secrets.json'
   SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
   SHEET_ID = '11TLb2eesRU5EC3N7fv6wX0ted5O2jik2T8SpM67PYm0'
   RANGE = "Sheet1!A2:B3"
 
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-        'secrets.json', SCOPES)
-      creds = flow.run_local_server(port=0)
-      # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-      token.write(creds.to_json())
+  creds = service_account.Credentials.from_service_account_file(
+    CREDENTIALS, scopes=SCOPES)
 
-    try:
-      service = build('sheets', 'v4', credentials=creds)
+  try:
+    service = build('sheets', 'v4', credentials=creds)
 
-        # Call the Sheets API
-      sheet = service.spreadsheets()
-      result = sheet.values().get(spreadsheetId=SHEET_ID,
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SHEET_ID,
                                     range=RANGE).execute()
-      values = result.get('values', [])
-
-      if not values:
-        print('No data found.')
-        return
-
-      print('Name, Major:')
-      for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s' % (row[0], row[4]))
-    except HttpError as err:
-        print(err)
+    values = result.get('values', [])
+    print(values)
+  except:
+    print('failed')
 
   return 'hi'
