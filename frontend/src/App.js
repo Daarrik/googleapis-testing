b727fa13c9798e04141373cad61f5c9f0842038c
Darrik Houck <darrikwh@gmail.com>
2022-04-19 13:18:24 -0700

    move 'sensitive' info to process.env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
-const spreadsheetId = "11TLb2eesRU5EC3N7fv6wX0ted5O2jik2T8SpM67PYm0";
-const range = "Sheet1!A2:B3";
-const apiKey = "AIzaSyB7bzY4r_lr7gjDCZ0fetgDjM2vjixKIPs";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(async () => {
     const response = await fetch(
-      `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`
+      `https://sheets.googleapis.com/v4/spreadsheets/${process.env.REACT_APP_SHEET_ID}/values/${process.env.REACT_APP_RANGE}?key=${process.env.REACT_APP_API_KEY}`
     );
     const { values } = await response.json();
     setMessages(values);
